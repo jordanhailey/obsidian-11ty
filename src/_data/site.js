c462e165e21b750eafa481f71ad8d5318445cddb
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-07-26 22:43:13 -0700

    edit page on github link

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,11 @@
-const VAULT_NAME = process.env.OBSIDIAN_VAULT_NAME;
+const {OBSIDIAN_VAULT_NAME:VAULT_NAME,GITHUB_REPO,GIT_BRANCH,GITHUB_REPO_IS_PUBLIC} = process.env;
 
 module.exports = {
 	title: VAULT_NAME,
 	description: "A place for my thoughts",
 	github_repo: {
-		base: "https://github.com/jordanhailey/jordanhailey",
-		branch: "main",
+		base: GITHUB_REPO,
+		branch: GIT_BRANCH,
+		isPublic:GITHUB_REPO_IS_PUBLIC
 	}
 }
